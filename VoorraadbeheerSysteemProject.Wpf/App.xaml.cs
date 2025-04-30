22e42b7d79ecb6452dcba6f3664dda91e37ce449
despeghelkobe <kobe.despeghel@student.vives.be>
2025-04-30 14:32:47 +0200

    basic setup of commands viewmodels and navigation

--- VoorraadbeheerSysteemProject.Wpf/App.xaml.cs
+++ VoorraadbeheerSysteemProject.Wpf/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.DirectoryServices.ActiveDirectory;
 using System.Windows;
+using VoorraadbeheerSysteemProject.Wpf.Stores;
+using VoorraadbeheerSysteemProject.Wpf.ViewModels;
 
 namespace VoorraadbeheerSysteemProject.Wpf;
 
@@ -9,5 +12,18 @@ namespace VoorraadbeheerSysteemProject.Wpf;
 /// </summary>
 public partial class App : Application
 {
+    NavigationStore _navigationStore = new NavigationStore();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _navigationStore.CurrentViewModel = new VmProducts(_navigationStore);
+
+        MainWindow = new MainWindow()
+        {
+            DataContext = new VmMainWindow(_navigationStore)
+        };
+        MainWindow.Show();
+        base.OnStartup(e);
+    }
 }
 
---
+++ VoorraadbeheerSysteemProject.Wpf/Commands/NavigationCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using VoorraadbeheerSysteemProject.Wpf.Stores;
+using VoorraadbeheerSysteemProject.Wpf.ViewModels;
+
+namespace VoorraadbeheerSysteemProject.Wpf.Commands
+{
+    public class NavigationCommand<TViewModel> : CommandBase
+       where TViewModel : VmBase
+    {
+        private readonly NavigationStore _navigationStore;
+
+        private Func<TViewModel> _createViewModel;
+
+        public NavigationCommand(NavigationStore navigationStore, Func<TViewModel> createViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+
+        public override void Execute(object? parameter)
+        {
+            _navigationStore.CurrentViewModel = _createViewModel();
+        }
+    }
+}
