73a3c636376c75ec32ba384e3f81cf0eba7fa48d
despeghelkobe <kobe.despeghel@student.vives.be>
2025-05-07 16:21:53 +0200

    made a custom combobox with filtering for products page

--- VoorraadbeheerSysteemProject.Wpf/ViewModels/VmProducts.cs
+++ VoorraadbeheerSysteemProject.Wpf/ViewModels/VmProducts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,11 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
 
             //NavigateDataCommand = new NavigationCommand<vmLogin>(navigationStore,
             //    () => new vmLogin(navigationStore));
+
+
+
+
+            //temp filling of products
             Products = new List<Product>()
             {
                 new Product()
@@ -63,9 +69,158 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
                     Barcode = "4567890123456"
                 }
             };
+
+
+            //temp filling of categories
+            AllCategories = new List<string> {
+                "no category selected",
+                "category 1",
+                "category 2",
+                "category 3",
+            };
+            FilteredCategories = AllCategories;
+
+            //temp filling of tax rates
+            AllTaxRate = new List<string> {
+                "no tax rate selected",
+                "6%",
+                "21%"
+            };
+            FilteredTaxRate = AllTaxRate;
+
+            //temp filling of shelves
+            AllShelf = new List<string> {
+                "no shelf selected",
+                "shelf 1",
+                "shelf 2",
+                "shelf 3"
+            };
+            FilteredShelf = AllShelf;
+
         }
 
         public IList<Product> Products { get; set; }
+
+
+        #region category filter
+        private string _searchTextCategories;
+        public string SearchTextCategories
+        {
+            get => _searchTextCategories;
+            set
+            {
+                _searchTextCategories = value;
+                OnPropertyChanged();
+                FilterCategories();
+            }
+        }
+
+        private IList<string> _allCategories;
+        public IList<string> AllCategories
+        {
+            get => _allCategories;
+            set { _allCategories = value; OnPropertyChanged(); }
+        }
+
+        private IList<string> _filteredCategories;
+        public IList<string> FilteredCategories
+        {
+            get => _filteredCategories;
+            set { _filteredCategories = value; OnPropertyChanged(); }
+        }
+
+        private void FilterCategories()
+        {
+            if (string.IsNullOrWhiteSpace(SearchTextCategories))
+                FilteredCategories = new List<string>(AllCategories ?? new List<string>());
+            else
+            {
+                FilteredCategories = AllCategories
+                    .Where(items => items.ToLower()
+                        .Contains(SearchTextCategories.ToLower()))
+                    .ToList();
+            }
+        }
+        #endregion
+
+        #region tax rate filter
+        private string _searchTextTaxRate;
+        public string SearchTextTaxRate
+        {
+            get => _searchTextTaxRate;
+            set
+            {
+                _searchTextTaxRate = value;
+                OnPropertyChanged();
+                FilterTaxRate();
+            }
+        }
+        private IList<string> _filteredTaxRate;
+        public IList<string> FilteredTaxRate
+        {
+            get => _filteredTaxRate;
+            set { _filteredTaxRate = value; OnPropertyChanged(); }
+        }
+
+        private IList<string> _allTaxRate;
+        public IList<string> AllTaxRate
+        {
+            get => _allTaxRate;
+            set { _allTaxRate = value; OnPropertyChanged(); }
+        }
+        public void FilterTaxRate()
+        {
+            if (string.IsNullOrWhiteSpace(SearchTextTaxRate))
+                FilteredTaxRate = new List<string>(AllTaxRate ?? new List<string>());
+            else
+            {
+                FilteredTaxRate = AllTaxRate
+                    .Where(items => items.ToLower()
+                        .Contains(SearchTextTaxRate.ToLower()))
+                    .ToList();
+            }
+        }
+        #endregion
+
+        #region Shelf filter
+        private string _searchTextShelf;
+        public string SearchTextShelf
+        {
+            get => _searchTextShelf;
+            set
+            {
+                _searchTextShelf = value;
+                OnPropertyChanged();
+                FilterShelf();
+            }
+        }
+        private IList<string> _filteredShelf;
+        public IList<string> FilteredShelf
+        {
+            get => _filteredShelf;
+            set { _filteredShelf = value; OnPropertyChanged(); }
+        }
+
+        private IList<string> _allShelf;
+        public IList<string> AllShelf
+        {
+            get => _allShelf;
+            set { _allShelf = value; OnPropertyChanged(); }
+        }
+        public void FilterShelf()
+        {
+            if (string.IsNullOrWhiteSpace(SearchTextShelf))
+                FilteredShelf = new List<string>(AllShelf ?? new List<string>());
+            else
+            {
+                FilteredShelf = AllShelf
+                    .Where(items => items.ToLower()
+                        .Contains(SearchTextShelf.ToLower()))
+                    .ToList();
+            }
+        }
+        #endregion
+
 
 
     }
