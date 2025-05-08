18321f9b3207bb27980f0375800208c367e3025f
Yussri-dev <ghanmiyoussri@gmail.com>
2025-05-08 14:53:31 +0200

    adding productDto, services and showing the data using Api Adress

---
+++ VoorraadbeheerSysteemProject.Wpf/Services/ApiService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using VoorraadbeheerSysteemProject.Wpf.Models;
+
+namespace VoorraadbeheerSysteemProject.Wpf.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl;
+        public ApiService(string baseUrl)
+        {
+            _httpClient = new HttpClient();
+            _baseUrl = baseUrl;
+            _httpClient.BaseAddress = new Uri(_baseUrl);
+        }
+
+        public async Task<List<ProductDTO>> GetProductsAsync()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("api/product");
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadFromJsonAsync<List<ProductDTO>>();
+            }
+            catch (Exception ex)
+            {
+                return new List<ProductDTO>();
+            }
+        }
+    }
+}
--- VoorraadbeheerSysteemProject.Wpf/ViewModels/VmSale.cs
+++ VoorraadbeheerSysteemProject.Wpf/ViewModels/VmSale.cs
@@ -1,17 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VoorraadbeheerSysteemProject.Wpf.Models;
+using VoorraadbeheerSysteemProject.Wpf.Services;
 using VoorraadbeheerSysteemProject.Wpf.Stores;
 
 namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
 {
     class VmSale : VmBase
     {
+        private readonly ApiService _apiService;
+        private readonly NavigationStore _navigationStore;
         public VmSale(NavigationStore navigationStore)
         {
+            _navigationStore = navigationStore;
+            _apiService = new ApiService("https://inventoryapi-dtavbdhhgdama7cr.switzerlandnorth-01.azurewebsites.net/");
 
+            Products = new ObservableCollection<ProductDTO>();
+            Task.Run(async () => await LoadDataAsync());
+        }
+
+        private ObservableCollection<ProductDTO> _products;
+
+        public ObservableCollection<ProductDTO> Products
+        {
+            get => _products;
+            set
+            {
+                _products = value;
+                OnPropertyChanged(nameof(Products));
+            }
+        }
+
+        private async Task LoadDataAsync()
+        {
+            var products = await _apiService.GetProductsAsync();
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                Products.Clear();
+                foreach (var product in products)
+                {
+                    Products.Add(product);
+                }
+            });
         }
     }
 }
