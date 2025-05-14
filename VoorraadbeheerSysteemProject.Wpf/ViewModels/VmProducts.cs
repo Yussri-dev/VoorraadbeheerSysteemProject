3c992b1fda80bdc0e1681f32c65f6e65760479fc
despeghelkobe <kobe.despeghel@student.vives.be>
2025-05-14 16:26:44 +0200

    reformed vmProducts for readability + added save button (no functionality)

--- VoorraadbeheerSysteemProject.Wpf/ViewModels/VmProducts.cs
+++ VoorraadbeheerSysteemProject.Wpf/ViewModels/VmProducts.cs
@@ -16,15 +16,39 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
     {
         private readonly ApiService _apiService;
 
+        //productDTO
         private ObservableCollection<ProductDTO> _products;
         private ObservableCollection<ProductDTO> _filteredProducts;
         private ProductDTO? _selectedProduct;
 
+        //categoryDTO
+        private ObservableCollection<string> _categories;
+        private ObservableCollection<string> _filteredCategories;
+
+        //TaxRateDTO
+        private ObservableCollection<string> _taxRates;
+        private ObservableCollection<string> _filteredTaxRate;
+
+        //ShelfDTO
+        private ObservableCollection<string> _shelfs;
+        private ObservableCollection<string> _filteredShelf;
+
+
+        //search/filter
         private string _searchTextName = "";
         private string _searchTextBarcode = "";
+        private string _searchTextTaxRate;
+        private string _searchTextShelf;
+        private string _searchTextCategories;
 
+
+
+        #region command properties
         public ICommand DisableOrEnableProductCommand { get; }
+        public ICommand SaveCommand { get; }
+        #endregion
 
+        #region Product properties
         public ObservableCollection<ProductDTO> Products { 
             get => _products; 
             set {
@@ -75,7 +99,51 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
         {
             get => _products?.Count ?? 0;
         }
+        #endregion
 
+        #region category properties
+        public ObservableCollection<string> AllCategories
+        {
+            get => _categories;
+            set { _categories = value; OnPropertyChanged(); }
+        }
+
+        public ObservableCollection<string> FilteredCategories
+        {
+            get => _filteredCategories;
+            set { _filteredCategories = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region tax rate properties
+        public ObservableCollection<string> FilteredTaxRate
+        {
+            get => _filteredTaxRate;
+            set { _filteredTaxRate = value; OnPropertyChanged(); }
+        }
+
+        public ObservableCollection<string> AllTaxRate
+        {
+            get => _taxRates;
+            set { _taxRates = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region shelf properties
+        public ObservableCollection<string> FilteredShelf
+        {
+            get => _filteredShelf;
+            set { _filteredShelf = value; OnPropertyChanged(); }
+        }
+
+        public ObservableCollection<string> AllShelf
+        {
+            get => _shelfs;
+            set { _shelfs = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region search/filter text properties
         public string SearchTextName
         {
             get { return _searchTextName; }
@@ -103,6 +171,41 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
             }
         }
 
+        public string SearchTextCategories
+        {
+            get => _searchTextCategories;
+            set
+            {
+                _searchTextCategories = value;
+                OnPropertyChanged();
+                FilterCategories();
+            }
+        }
+
+        public string SearchTextTaxRate
+        {
+            get => _searchTextTaxRate;
+            set
+            {
+                _searchTextTaxRate = value;
+                OnPropertyChanged();
+                FilterTaxRate();
+            }
+        }
+
+        public string SearchTextShelf
+        {
+            get => _searchTextShelf;
+            set
+            {
+                _searchTextShelf = value;
+                OnPropertyChanged();
+                FilterShelf();
+            }
+        }
+        #endregion
+
+
         #region constructors
         public VmProducts(NavigationStore navigationStore)
         {
@@ -120,144 +223,8 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
         }
         #endregion
 
-        #region category filter
-        private string _searchTextCategories;
-        public string SearchTextCategories
-        {
-            get => _searchTextCategories;
-            set
-            {
-                _searchTextCategories = value;
-                OnPropertyChanged();
-                FilterCategories();
-            }
-        }
 
-        private IList<string> _allCategories;
-        public IList<string> AllCategories
-        {
-            get => _allCategories;
-            set { _allCategories = value; OnPropertyChanged(); }
-        }
-
-        private IList<string> _filteredCategories;
-        public IList<string> FilteredCategories
-        {
-            get => _filteredCategories;
-            set { _filteredCategories = value; OnPropertyChanged(); }
-        }
-
-        private void FilterCategories()
-        {
-            if (string.IsNullOrWhiteSpace(SearchTextCategories))
-                FilteredCategories = new List<string>(AllCategories ?? new List<string>());
-            else
-            {
-                FilteredCategories = AllCategories
-                    .Where(items => items.ToLower()
-                        .Contains(SearchTextCategories.ToLower()))
-                    .ToList();
-            }
-        }
-        #endregion
-
-        #region tax rate filter
-        private string _searchTextTaxRate;
-        public string SearchTextTaxRate
-        {
-            get => _searchTextTaxRate;
-            set
-            {
-                _searchTextTaxRate = value;
-                OnPropertyChanged();
-                FilterTaxRate();
-            }
-        }
-        private IList<string> _filteredTaxRate;
-        public IList<string> FilteredTaxRate
-        {
-            get => _filteredTaxRate;
-            set { _filteredTaxRate = value; OnPropertyChanged(); }
-        }
-
-        private IList<string> _allTaxRate;
-        public IList<string> AllTaxRate
-        {
-            get => _allTaxRate;
-            set { _allTaxRate = value; OnPropertyChanged(); }
-        }
-        public void FilterTaxRate()
-        {
-            if (string.IsNullOrWhiteSpace(SearchTextTaxRate))
-                FilteredTaxRate = new List<string>(AllTaxRate ?? new List<string>());
-            else
-            {
-                FilteredTaxRate = AllTaxRate
-                    .Where(items => items.ToLower()
-                        .Contains(SearchTextTaxRate.ToLower()))
-                    .ToList();
-            }
-        }
-        #endregion
-
-        #region Shelf filter
-        private string _searchTextShelf;
-        public string SearchTextShelf
-        {
-            get => _searchTextShelf;
-            set
-            {
-                _searchTextShelf = value;
-                OnPropertyChanged();
-                FilterShelf();
-            }
-        }
-        private IList<string> _filteredShelf;
-        public IList<string> FilteredShelf
-        {
-            get => _filteredShelf;
-            set { _filteredShelf = value; OnPropertyChanged(); }
-        }
-
-        private IList<string> _allShelf;
-        public IList<string> AllShelf
-        {
-            get => _allShelf;
-            set { _allShelf = value; OnPropertyChanged(); }
-        }
-        public void FilterShelf()
-        {
-            if (string.IsNullOrWhiteSpace(SearchTextShelf))
-                FilteredShelf = new List<string>(AllShelf ?? new List<string>());
-            else
-            {
-                FilteredShelf = AllShelf
-                    .Where(items => items.ToLower()
-                        .Contains(SearchTextShelf.ToLower()))
-                    .ToList();
-            }
-        }
-        #endregion
-
-
-
-        #region Methods
-
-
-        private void DisableOrEnableProduct(object parameter)
-        {
-            if(_selectedProduct == null)
-                return;
-
-            _selectedProduct.IsActivate = !_selectedProduct.IsActivate;
-            _products[_products.IndexOf(_selectedProduct)] = _selectedProduct;
-            FilterProducts();
-            OnPropertyChanged(nameof(ProductIsActive));
-            //OnPropertyChanged(nameof(SelectedProduct));
-            //OnPropertyChanged(nameof(FilteredProducts));
-        }
-
-
+        #region filter methods
         private void FilterProducts()
         {
             var filterdProducts = Products.AsEnumerable();
@@ -269,6 +236,76 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
                 filterdProducts = filterdProducts?.Where(p => p.Barcode.ToLower().Contains(SearchTextBarcode.ToLower()));
 
             FilteredProducts = new ObservableCollection<ProductDTO>(filterdProducts);
+        }
+
+        private void FilterCategories()
+        {
+            if (string.IsNullOrWhiteSpace(_searchTextCategories))
+                FilteredCategories = new ObservableCollection<string>(
+                    _categories ?? new ObservableCollection<string>());
+            else
+            {
+                FilteredCategories = new ObservableCollection<string>(
+                    _categories.AsEnumerable()
+                    .Where(
+                        items => items.ToLower()
+                        .Contains(_searchTextCategories.ToLower())
+                    )
+                    .ToList()
+                );
+            }
+        }
+
+        public void FilterTaxRate()
+        {
+            if (string.IsNullOrWhiteSpace(_searchTextTaxRate))
+                FilteredTaxRate = new ObservableCollection<string>(
+                    _taxRates ?? new ObservableCollection<string>());
+            else
+            {
+                FilteredTaxRate = new ObservableCollection<string>(
+                    _taxRates.AsEnumerable()
+                    .Where(
+                        items => items.ToLower()
+                        .Contains(_searchTextTaxRate.ToLower())
+                    )
+                    .ToList()
+                );
+            }
+        }
+
+        public void FilterShelf()
+        {
+            if (string.IsNullOrWhiteSpace(_searchTextShelf))
+                FilteredShelf = new ObservableCollection<string>(
+                    _shelfs ?? new ObservableCollection<string>());
+            else
+            {
+                FilteredShelf = new ObservableCollection<string>(
+                    _shelfs
+                    .Where(
+                        items => items.ToLower()
+                        .Contains(_searchTextShelf.ToLower())
+                    )
+                    .ToList()
+                );
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        private void DisableOrEnableProduct(object parameter)
+        {
+            if (_selectedProduct == null)
+                return;
+
+            _selectedProduct.IsActivate = !_selectedProduct.IsActivate;
+            _products[_products.IndexOf(_selectedProduct)] = _selectedProduct;
+            FilterProducts();
+            OnPropertyChanged(nameof(ProductIsActive));
+            //OnPropertyChanged(nameof(SelectedProduct));
+            //OnPropertyChanged(nameof(FilteredProducts));
         }
 
         private async Task LoadDataAsync()
@@ -290,7 +327,7 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
             FilteredProducts = new ObservableCollection<ProductDTO>(Products.OrderBy(p => p.Name).ToList());
 
             //temp filling of categories
-            AllCategories = new List<string> {
+            AllCategories = new ObservableCollection<string> {
                 "no category selected",
                 "category 1",
                 "category 2",
@@ -299,7 +336,7 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
             FilteredCategories = AllCategories;
 
             //temp filling of tax rates
-            AllTaxRate = new List<string> {
+            AllTaxRate = new ObservableCollection<string> {
                 "no tax rate selected",
                 "6%",
                 "21%"
@@ -307,7 +344,7 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
             FilteredTaxRate = AllTaxRate;
 
             //temp filling of shelves
-            AllShelf = new List<string> {
+            AllShelf = new ObservableCollection<string> {
                 "no shelf selected",
                 "shelf 1",
                 "shelf 2",
