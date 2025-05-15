748f2d09bbbc9bd341f358d14925f020ab7c2e47
Yussri-dev <ghanmiyoussri@gmail.com>
2025-05-15 12:12:33 +0200

    Add Amount to View

---
+++ VoorraadbeheerSysteemProject.Wpf/Requests/SaleSelectedAmountRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoorraadbeheerSysteemProject.Wpf.Requests
+{
+    class SaleSelectedAmountRequest : INotifyPropertyChanged
+    {
+
+        private decimal _amountPrice { get; set; }
+        public decimal AmountPrice
+        {
+            get => _amountPrice;
+            set
+            {
+                if (_amountPrice != value)
+                {
+                    _amountPrice = value;
+                    OnPropertyChanged(nameof(AmountPrice));
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
