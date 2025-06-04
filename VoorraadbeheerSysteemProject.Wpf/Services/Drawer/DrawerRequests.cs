60664579a052b033fd56ffb9e96283bb240b8747
Yussri-dev <ghanmiyoussri@gmail.com>
2025-06-04 15:57:34 +0200

    add drawer

---
+++ VoorraadbeheerSysteemProject.Wpf/Services/Drawer/DrawerRequests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using VoorraadbeheerSysteemProject.Wpf.Models;
+
+namespace VoorraadbeheerSysteemProject.Wpf.Services.Drawer
+{
+    class DrawerRequests
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl;
+
+        public DrawerRequests(string baseUrl)
+        {
+            _httpClient = new HttpClient();
+            _baseUrl = baseUrl;
+            _httpClient.BaseAddress = new Uri(_baseUrl);
+        }
+
+        public async Task<CashShiftDTO> PostCashShiftAsync(CashShiftDTO cashShift)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/cashshift", cashShift);
+                if (response.IsSuccessStatusCode)
+                {
+                    var createdCashShift = await response.Content.ReadFromJsonAsync<CashShiftDTO>();
+                    return createdCashShift;
+                }
+                else
+                {
+                    var errorContent = await response.Content.ReadAsStringAsync();
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ VoorraadbeheerSysteemProject.Wpf/ViewModels/VmDrawer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using VoorraadbeheerSysteemProject.Wpf.Commands;
+using VoorraadbeheerSysteemProject.Wpf.Commands.DrawerCommands;
+using VoorraadbeheerSysteemProject.Wpf.Models;
+using VoorraadbeheerSysteemProject.Wpf.Services.Drawer;
+using VoorraadbeheerSysteemProject.Wpf.Services.SaasClients;
+using VoorraadbeheerSysteemProject.Wpf.Stores;
+
+namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
+{
+    class VmDrawer : VmBase
+    {
+        private readonly DrawerRequests _drawerRequest;
+        private readonly NavigationStore _navigationStore;
+        public VmDrawer(NavigationStore navigationStore)
+        {
+            validateCashShiftCommand = new ValidateCashShiftCommand(this);
+        }
+
+        #region Fields & commands
+
+        private DateTime _shiftStart;
+        public DateTime ShiftStart
+        {
+            get => _shiftStart;
+            set
+            {
+                _shiftStart = value;
+            }
+        }
+
+        private string _statusMessage;
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            set
+            {
+                _statusMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ICommand validateCashShiftCommand { get; set; }
+
+        #endregion
+        public async Task<CashShiftDTO> SaveCashShiftAsync(CashShiftDTO cashShiftDto)
+        {
+            try
+            {
+                var createdCashShift = await _drawerRequest.PostCashShiftAsync(cashShiftDto);
+                return createdCashShift;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        #region Methods
+        private async Task RegisterCashShiftAsync()
+        {
+            StatusMessage = string.Empty;
+
+            if (
+                string.IsNullOrWhiteSpace(FullName) ||
+                string.IsNullOrWhiteSpace(Email) ||
+                string.IsNullOrWhiteSpace(Adresse) ||
+                string.IsNullOrWhiteSpace(SubscriptionType)
+                )
+            {
+                StatusMessage = "All Fields Are required";
+            }
+            var responseDto = new CashShiftDTO
+            {
+                CashRegisterId = 3,
+
+            };
+
+            await _drawerRequest.PostCashShiftAsync(responseDto);
+        }
+        #endregion
+    }
+}
