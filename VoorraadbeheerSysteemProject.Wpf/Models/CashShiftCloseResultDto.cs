8a93bb549e8beda463139292ed1fc39f49fae729
Yussri-dev <ghanmiyoussri@gmail.com>
2025-06-04 10:10:02 +0200

    add CashShiftCloseResultDto

---
+++ VoorraadbeheerSysteemProject.Wpf/Models/CashShiftCloseResultDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoorraadbeheerSysteemProject.Wpf.Models
+{
+    public class CashShiftCloseResultDto
+    {
+        public decimal Expected { get; set; }
+        public decimal Actual { get; set; }
+        public decimal Difference => Actual - Expected;
+    }
+}
