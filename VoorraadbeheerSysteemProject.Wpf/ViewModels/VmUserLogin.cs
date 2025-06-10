3908932c81c7e887eb552846ed4ef9fda6807bdc
Yussri-dev <ghanmiyoussri@gmail.com>
2025-06-10 22:39:32 +0200

    add buttons Add and Minus in Sales and Purchases

--- VoorraadbeheerSysteemProject.Wpf/ViewModels/VmUserLogin.cs
+++ VoorraadbeheerSysteemProject.Wpf/ViewModels/VmUserLogin.cs
@@ -60,8 +60,8 @@ namespace VoorraadbeheerSysteemProject.Wpf.ViewModels
         {
             StatusMessage = string.Empty;
 
-            UserName = "ghanmiyoussri@gmail.com";
-            //UserName = "ghanmi.youssri.dev@gmail.com";
+            //UserName = "ghanmiyoussri@gmail.com";
+            UserName = "ghanmi.youssri.dev@gmail.com";
             Password = "077671699Yos@";
             if (string.IsNullOrWhiteSpace(UserName) || string.IsNullOrWhiteSpace(Password))
             {
