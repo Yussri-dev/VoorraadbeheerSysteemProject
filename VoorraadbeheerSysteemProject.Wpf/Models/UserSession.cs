1172ca819b79da29d49ebc8b9d949798f5dac158
Yussri-dev <ghanmiyoussri@gmail.com>
2025-06-12 10:07:18 +0200

    Adding Roles and updating Style in Quantity Sales and Purchases

--- VoorraadbeheerSysteemProject.Wpf/Models/UserSession.cs
+++ VoorraadbeheerSysteemProject.Wpf/Models/UserSession.cs
@@ -12,11 +12,14 @@ namespace VoorraadbeheerSysteemProject.Wpf.Models
         public static string? Token { get; set; }
         public static string? IdUSer { get; set; }
         public static int IdSaasClient { get; set; }
+        public static List<string> Roles { get; set; }
+
         public static void Clear()
         {
             Email = null;
             Token = null;
             IdUSer = null;
+            Roles.Clear();
         }
     }
 }
